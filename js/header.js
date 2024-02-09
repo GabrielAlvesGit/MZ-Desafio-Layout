99e92ef74b75a78c18c8d1c7b7b6f31f1bb2471d
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2024-02-09 15:55:49 -0300

    Add  sobre nossos fundos

--- js/header.js
+++ js/header.js
@@ -1,8 +1,12 @@
-/*=============== Menu header hamburger ===============*/
+var body = document.body;
 var nav = document.querySelector(".menu--nav");
 var menuHamburger = document.querySelector(".menu-hamburger");
 var menuIcon = document.getElementById("menu-icon");
 var closeIcon = document.getElementById("close-icon");
+var comoInvestirBtn = document.querySelector(".btns-heard-comoInv");
+var comoInvestirContainer = document.querySelector(
+    ".btns-heard-comoInv-container"
+);
 
 function toggleMenu() {
     if (nav.classList.contains("active")) {
@@ -10,10 +14,14 @@ function toggleMenu() {
         menuHamburger.setAttribute("aria-expanded", "false");
         menuIcon.style.display = "inline";
         closeIcon.style.display = "none";
+        comoInvestirContainer.style.display = "none";
+        body.style.overflow = "";
     } else {
         nav.classList.add("active");
         menuHamburger.setAttribute("aria-expanded", "true");
         menuIcon.style.display = "none";
         closeIcon.style.display = "inline";
+        comoInvestirContainer.style.display = "block";
+        body.style.overflow = "hidden";
     }
 }
