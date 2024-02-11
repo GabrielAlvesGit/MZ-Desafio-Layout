6d040e2015eaaa9ac713cc0df26d005754947b12
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2024-02-11 19:33:44 -0300

    Create funcionalidade de atualização do carrossel e integração de dados a partir do arquivo JSON Create layout da seção 'Novidades' Create layout da seção 'Rodapé/footer' Add  imagens ao projeto

--- js/header.js
+++ js/header.js
@@ -8,6 +8,13 @@ var comoInvestirContainer = document.querySelector(
     ".btns-heard-comoInv-container"
 );
 
+var divCarousel = document.getElementById("carousel");
+var descricaoElement = divCarousel.querySelector("#textCarousel");
+var imgElement = divCarousel.querySelector("#imgCarousel");
+var titleElement = divCarousel.querySelector("#titleCarousel");
+
+let currentIndex = 0;
+
 function toggleMenu() {
     if (nav.classList.contains("active")) {
         nav.classList.remove("active");
@@ -25,3 +32,50 @@ function toggleMenu() {
         body.style.overflow = "hidden";
     }
 }
+
+/* =============== Carousel ===============*/
+function updateCarousel(index) {
+    titleElement.textContent = dados.carousel[index].title;
+    descricaoElement.textContent = dados.carousel[index].descricao;
+    imgElement.src = dados.carousel[index].img;
+    imgElement.alt = dados.carousel[index].alt;
+
+    // Remove todos os dots
+    document.querySelectorAll(".dot").forEach((dot) => {
+        dot.classList.remove("special-dot");
+    });
+
+    // Adiciona dot clicado
+    document
+        .querySelector(`.dot:nth-child(${index + 1})`)
+        .classList.add("special-dot");
+}
+
+document
+    .querySelector(".carousel-btn:first-child")
+    .addEventListener("click", function () {
+        currentIndex =
+            (currentIndex - 1 + dados.carousel.length) % dados.carousel.length;
+        updateCarousel(currentIndex);
+    });
+
+document
+    .querySelector(".carousel-btn:last-child")
+    .addEventListener("click", function () {
+        currentIndex = (currentIndex + 1) % dados.carousel.length;
+        updateCarousel(currentIndex);
+    });
+
+document.querySelectorAll(".dot").forEach(function (dot, index) {
+    dot.addEventListener("click", function () {
+        currentIndex = index;
+        updateCarousel(currentIndex);
+    });
+});
+
+fetch("./js/Data.json").then((response) => {
+    response.json().then((data) => {
+        dados = data;
+        updateCarousel(currentIndex);
+    });
+});
